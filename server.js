35e3c1f68bd4ffe2787213e1a7849b0355ce2f2f
David N <bigair_1080@hotmail.com>
2019-02-23 22:48:00 -0400

    changed cors location

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ const db = knex({
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => { res.send('it is working') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
