56bdb47e8dd7a32caa4bfb1a1aa50148ed4aacf5
David N <bigair_1080@hotmail.com>
2019-03-27 20:09:46 -0300

    updated to included pinging of app

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const https = require('https');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -28,6 +29,10 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res, db) })
+
+setInterval(() => {
+  https.get("https://smart-brain-dpn.herokuapp.com");
+}, 1500000);
 
 
 app.listen(process.env.PORT || 3000, () => {
