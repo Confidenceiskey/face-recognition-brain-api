9b4d15263c336a2c830e3b111bea2f9110ad3ec6
David N <bigair_1080@hotmail.com>
2019-03-29 12:23:00 -0300

    edited server file

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const https = require('https');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -29,11 +28,6 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res, db) })
-
-setInterval(() => {
-  https.get("https://smart-brain-dpn.herokuapp.com");
-}, 1500000);
-
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port ${process.env.PORT}`);
