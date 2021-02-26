2e6a6832fa73eb4f7a3b10b970c7ba6b33e1ae67
David N <bigair_1080@hotmail.com>
2021-02-26 18:15:20 +0100

    update port entry

--- server.js
+++ server.js
@@ -30,5 +30,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req,res) => { image.handleApiCall(req, res, db) })
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App is running on port ${process.env.PORT}`);
+  console.log(`App is running on port ${process.env.PORT || 3000}`);
 })
