9d7f50bfeb5f64120fdaea245a2d12d708b995e5
David N <bigair_1080@hotmail.com>
2021-02-27 10:56:43 +0100

    update server info

--- server.js
+++ server.js
@@ -1,34 +1,53 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL, //in other words this is localhost 1.27.0.1 (not sure on exact name)
-    ssl: true
-  }
+    ssl: true,
+  },
 });
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use(cors({origin: ['https://smart-brain-dpn.herokuapp.com', 'https://face-brain-dpn.herokuapp.com']}));
+app.use(
+  cors({
+    origin: [
+      "https://smart-brain-dpn.herokuapp.com",
+      "https://face-brain-dpn.herokuapp.com",
+    ],
+    allowedHeaders: ['Access-Control-Allow-Origin', 'https://smart-brain-dpn.herokuapp.com'],
+    credentials: true,
+  })
+);
 
-app.get('/', (req, res) => { res.send('it is working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req,res) => { image.handleApiCall(req, res, db) })
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res, db);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App is running on port ${process.env.PORT || 3000}`);
-})
+});
